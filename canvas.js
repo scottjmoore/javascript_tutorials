2011ad7a695ff0a1baf89b516aa0e9ab0fe708d0
Scott Moore (Scott Moore Software) <scott.moore@scottmooresoftware.com>
2019-06-05 22:55:48 +0100

    Add some canvas script

--- canvas.js
+++ canvas.js
@@ -1,1 +1,12 @@
 console.log(`JavaScript Canvas`);
+
+var canvas = document.getElementById('mycanvas');
+
+if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = 'red';
+    ctx.fillRect(0, 0, 512, 512);
+} else {
+    console.log('Failed to get canvas context');
+}
