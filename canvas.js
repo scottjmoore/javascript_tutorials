3c3c8c6811b2cd2bd76cdb51c42ec9b6c5a45f93
Scott Moore <scott.moore@scottmooresoftware.com>
2019-06-08 20:47:28 +0100

    Draw image

--- canvas.js
+++ canvas.js
@@ -1,12 +1,14 @@
 console.log(`JavaScript Canvas`);
 
 var canvas = document.getElementById('mycanvas');
+const img = getElementById('myimg');
 
 if (canvas.getContext) {
     var ctx = canvas.getContext('2d');
 
     ctx.fillStyle = 'blue';
     ctx.fillRect(0, 0, 512, 512);
+    ctx.drawImage(img, 0, 0);
 } else {
     console.log('Failed to get canvas context');
 }
