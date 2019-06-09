9f793bb8e92a2f367cd7e04fb9df1e1f0b90a309
Scott Moore <scott.moore@scottmooresoftware.com>
2019-06-09 22:40:54 +0100

    Move sprite draw into function

--- build/canvas.js
+++ build/canvas.js
@@ -4,41 +4,39 @@ const TO_RADIANS = Math.PI/180;
 const canvas = document.getElementById('mycanvas');
 const img = new Image();
 
-const url = "https://js.scottmooresoftware.com/.netlify/functions/test"
+var drawMarios = (rotation) => {
+    if (canvas.getContext) {
+        const ctx = canvas.getContext('2d');
 
-fetch(url)
-    .then(data => { return data.json()})
-    .then(res => { console.log(res)});
+        ctx.fillStyle = '#ccc';
+        ctx.fillRect(0, 0, 512, 512);
 
-if (canvas.getContext) {
-    const ctx = canvas.getContext('2d');
+        img.onload = function () {
+            ctx.save();
+            ctx.translate(64, 64);
+            ctx.rotate(45 * TO_RADIANS);
+            ctx.drawImage(img, -64, -64, 128, 128);
+            ctx.restore();
+            ctx.save();
+            ctx.translate(256, 256);
+            ctx.rotate(135 * TO_RADIANS);
+            ctx.drawImage(img, -64, -64, 128, 128);
+            ctx.restore();
+            ctx.save();
+            ctx.translate(64, 256);
+            ctx.rotate(225 * TO_RADIANS);
+            ctx.drawImage(img, -64, -64, 128, 128);
+            ctx.restore();
+            ctx.save();
+            ctx.translate(256,64);
+            ctx.rotate(315 * TO_RADIANS);
+            ctx.drawImage(img, -64, -64, 128, 128);
+            ctx.restore();
+        }
 
-    ctx.fillStyle = '#ccc';
-    ctx.fillRect(0, 0, 512, 512);
-
-    img.onload = function () {
-        ctx.save();
-        ctx.translate(64, 64);
-        ctx.rotate(45 * TO_RADIANS);
-        ctx.drawImage(img, -64, -64, 128, 128);
-        ctx.restore();
-        ctx.save();
-        ctx.translate(256, 256);
-        ctx.rotate(135 * TO_RADIANS);
-        ctx.drawImage(img, -64, -64, 128, 128);
-        ctx.restore();
-        ctx.save();
-        ctx.translate(64, 256);
-        ctx.rotate(225 * TO_RADIANS);
-        ctx.drawImage(img, -64, -64, 128, 128);
-        ctx.restore();
-        ctx.save();
-        ctx.translate(256,64);
-        ctx.rotate(315 * TO_RADIANS);
-        ctx.drawImage(img, -64, -64, 128, 128);
-        ctx.restore();
+        img.src = "img/mario.png";
     }
-
-    img.src = "img/mario.png";
 }
+
+drawMarios(1);
   
