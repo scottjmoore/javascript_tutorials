8b2d4cb33a6df40e247fb21bc030b8465eca8f4f
Scott Moore <scott.moore@scottmooresoftware.com>
2019-06-09 22:49:09 +0100

    Clean up draw marios function

--- build/canvas.js
+++ build/canvas.js
@@ -2,39 +2,33 @@ console.log(`JavaScript Canvas`);
 
 const TO_RADIANS = Math.PI/180;
 const canvas = document.getElementById('mycanvas');
+const ctx = canvas.getContext('2d');
 const img = new Image();
 
 var drawMarios = (rotation) => {
-    if (canvas.getContext) {
-        const ctx = canvas.getContext('2d');
-
+    if (ctx) {
         ctx.fillStyle = '#ccc';
         ctx.fillRect(0, 0, 512, 512);
-
-        img.onload = function () {
-            ctx.save();
-            ctx.translate(64, 64);
-            ctx.rotate((45 + rotation) * TO_RADIANS);
-            ctx.drawImage(img, -64, -64, 128, 128);
-            ctx.restore();
-            ctx.save();
-            ctx.translate(256, 256);
-            ctx.rotate((135 + rotation) * TO_RADIANS);
-            ctx.drawImage(img, -64, -64, 128, 128);
-            ctx.restore();
-            ctx.save();
-            ctx.translate(64, 256);
-            ctx.rotate((225 + rotation) * TO_RADIANS);
-            ctx.drawImage(img, -64, -64, 128, 128);
-            ctx.restore();
-            ctx.save();
-            ctx.translate(256,64);
-            ctx.rotate((315 + rotation) * TO_RADIANS);
-            ctx.drawImage(img, -64, -64, 128, 128);
-            ctx.restore();
-        }
-
-        img.src = "img/mario.png";
+        ctx.save();
+        ctx.translate(64, 64);
+        ctx.rotate((45 + rotation) * TO_RADIANS);
+        ctx.drawImage(img, -64, -64, 128, 128);
+        ctx.restore();
+        ctx.save();
+        ctx.translate(256, 256);
+        ctx.rotate((135 + rotation) * TO_RADIANS);
+        ctx.drawImage(img, -64, -64, 128, 128);
+        ctx.restore();
+        ctx.save();
+        ctx.translate(64, 256);
+        ctx.rotate((225 + rotation) * TO_RADIANS);
+        ctx.drawImage(img, -64, -64, 128, 128);
+        ctx.restore();
+        ctx.save();
+        ctx.translate(256,64);
+        ctx.rotate((315 + rotation) * TO_RADIANS);
+        ctx.drawImage(img, -64, -64, 128, 128);
+        ctx.restore();
     }
 }
 
@@ -45,7 +39,7 @@ var gameLoop = () => {
     drawMarios(rotation++);
 }
 
-window.requestAnimationFrame(gameLoop);
-
+img.src = "img/mario.png";
+gameLoop();
 
   
